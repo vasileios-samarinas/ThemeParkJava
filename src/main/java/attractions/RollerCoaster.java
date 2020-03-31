d60d8cd30829e7e1a25019a59a300e00f361567e
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-03-31 18:48:05 +0100

    Rollercoster Isecurity test passed

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,16 @@
 package attractions;
 
+import people.Visitor;
+
 public class RollerCoaster  extends Attraction {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
     }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        if(visitor.getAge() > 12 && visitor.getHeight()>1.45)
+        {return true;}
+        else {return false;}
+    };
 }
--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,12 +2,14 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor;
 
     @Before
     public void setUp() {
@@ -28,4 +30,24 @@ public class RollercoasterTest {
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
     }
+
+    @Test
+    public void hasAllowedVisitorHasBothValidHeightAndAge(){
+        visitor= new Visitor(26,1.75,100.00);
+        assertTrue(rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void hasNotAllowedVisitorHasValidHeightAndNotAge(){
+        visitor= new Visitor(11,1.75,100.00);
+        assertFalse(rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void hasNotAllowedVisitorHasNotValidHeightButAge(){
+        visitor= new Visitor(26,1.3,100.00);
+        assertFalse(rollerCoaster.isAllowedTo(visitor));
+    }
+
+
 }
